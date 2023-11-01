f9a0ceba2ad176a7990d569835b7d9b5465b7d1a
Katerina <katerina.tashoska@oblcc.com>
2023-11-01 15:59:33 +0100

    Readme

--- lambdas/triggered_by_event.mjs
+++ lambdas/triggered_by_event.mjs
@@ -3,6 +3,6 @@
     console.log("EVENT: \n" + JSON.stringify(event, null, 2));
     return {
         'statusCode': 200,
-        'body': JSON.stringify("Hello there")
+        'body': JSON.stringify("Hello from the app")
     };
   };
