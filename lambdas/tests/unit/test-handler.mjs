8e3101c901b4fc5a0350a530f0d74dc801d0b4b0
Katerina <katerina.tashoska@oblcc.com>
2023-11-07 13:21:32 +0100

    Pipeline

--- lambdas/tests/unit/test-handler.mjs
+++ lambdas/tests/unit/test-handler.mjs
@@ -1,21 +1,33 @@
 'use strict';
 
-import { handler } from '../../triggered_by_event.mjs';
+import { handler } from '../../put_event.mjs';
 import { expect } from 'chai';
 var event, context;
 
 describe('Tests trigger event', function () {
     it('verifies successful response', async () => {
+
+        event = {
+            body: '{\n' +
+                '  "configuration-id":"0671F841-03A4-4CC6-ABFF-9B5CC8610938",\n' +
+                '  "object-ids": [\n' +
+                '      101,\n' +
+                '      108,\n' +
+                '      223,\n' +
+                '      444\n' +
+                '  ]\n' +
+                '}\n'
+        };
         const result = await handler(event, context)
 
         console.log(result);
 
         expect(result).to.be.an('object');
-        //expect(result.statusCode).to.equal(200);
-        //expect(result.body).to.be.an('string');
+        expect(result.statusCode).to.equal(200);
+        expect(result.body).to.be.an('string');
 
-        //let response = JSON.parse(result.body);
+        let response = JSON.parse(result.body);
 
-        //expect(response).to.be.an('object');
+        expect(response).to.be.an('object');
     });
 });
