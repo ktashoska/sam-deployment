41481c8ad2fbff4103267ce1171c747c4c5fd141
Katerina <katerina.tashoska@xebia.com>
2023-11-08 09:05:59 +0100

    Initial EBS phase 1

---
+++ lambdas/triggered_by_event.mjs
@@ -0,0 +1,25 @@
+import { S3Client, CopyObjectCommand } from "@aws-sdk/client-s3";
+const client = new S3Client({});
+
+export const handler = async (event, context) => {
+    const originalPath = event.detail["original-path"];
+    const imageId = "flower1.jpg";//event.detail["image-id"];
+
+    // Extracting the source bucket from the original path
+    const sourceBucket = originalPath.split("/")[2]; 
+    const destinationBucket = 'qonqord-destination-bucket';
+
+    const command = new CopyObjectCommand({
+        CopySource: `${sourceBucket}/images/${imageId}`,
+        Bucket: destinationBucket,
+        Key: `images/${imageId}`,
+      });
+    
+      try {
+        const response = await client.send(command);
+        console.log(response);
+      } catch (err) {
+        console.error(err);
+      }
+
+};
